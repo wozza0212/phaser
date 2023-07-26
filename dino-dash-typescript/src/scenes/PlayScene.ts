39e03a8a16ed3c786011f0ff00f4833e7a8da7ea
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-07-26 18:39:48 +0100

    added gravity and worldcollide and bodysize

--- dino-dash-typescript/src/scenes/PlayScene.ts
+++ dino-dash-typescript/src/scenes/PlayScene.ts
@@ -22,6 +22,12 @@ class PlayScene extends Phaser.Scene {
         this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle')
         .setOrigin(0, 1)
 
+        this.player
+            .setGravityY(5000)
+            .setCollideWorldBounds(true)
+            .setBodySize(44, 92)
+                
+
     }
 
     createEnvironment() {
