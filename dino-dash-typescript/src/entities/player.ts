8c33cf19d865697fc1ff3b4ccc3dbc1477857296
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-08-01 18:31:26 +0100

    fix initial jump

--- dino-dash-typescript/src/entities/player.ts
+++ dino-dash-typescript/src/entities/player.ts
@@ -7,7 +7,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
 
     constructor(scene: GameScene, x: number, y:number) {
-        super(scene, x, y, 'dino-idle')
+        super(scene, x, y, 'dino-run')
         scene.add.existing(this)
         scene.physics.add.existing(this)
 
