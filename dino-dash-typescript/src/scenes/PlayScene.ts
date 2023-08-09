39ee1f8b1edce019b3ee6de1f694637044d7fe64
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-08-09 20:34:04 +0100

    restart after death

--- dino-dash-typescript/src/scenes/PlayScene.ts
+++ dino-dash-typescript/src/scenes/PlayScene.ts
@@ -93,7 +93,14 @@ class PlayScene extends GameScene {
 
     handleGameRestart() {
         this.restartText.on('pointerdown', () => {
-            console.log('restart');
+            this.physics.resume();
+            this.player.setVelocityY(0)
+
+            this.obstacles.clear(true, true);
+            this.gameOverContainer.setAlpha(0);
+            this.anims.resumeAll();
+
+            this.isGameRunning = true;
         })
 
     }
